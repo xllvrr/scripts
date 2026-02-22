84ac03843da1b0aea7077d1a79911533f598221a
xllvrr <dtsa.tan@gmail.com>
2026-02-22 22:32:47 +0800

    Changed cava to 20%

--- tmux-music.sh
+++ tmux-music.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-tmux has-session -t music 2>/dev/null || \
+tmux has-session -t music 2>/dev/null ||
     tmux new-session -d -s music -n "rmpc" "rmpc" \; \
-        split-window -v -l 30% "cava" \; \
-        select-pane -t 0 \; \
         new-window -n "spotify" "spotify_player"
 
 tmux attach -t music
